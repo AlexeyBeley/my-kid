d8268bed09325d20796d84c7c8a60403c528c1ae
Alexey <alexey.beley@gmail.com>
2020-05-08 12:44:38 +0300

    change db

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ sudo chmod 777 /var/run/docker.sock
 
 export SERVER_NAME="my-kid.co.il"
 export DBPASSWD=''
-export DBHOST='phpbb.crgmvorrv472.us-east-2.rds.amazonaws.com'
+export DBHOST='phpbb-db.crgmvorrv472.us-east-2.rds.amazonaws.com'
 export DBPORT='3306'
-export DBNAME='phpbb_db'
-export DBUSER='phpbb_user'
+export DBNAME='phpbb_db_prod'
+export DBUSER='phpbb_user_prod'
 export TABLE_PREFIX='phpbb_'
 export PHPBB_INSTALLED=true
 export AUTO_DB_MIGRATE=false
@@ -31,6 +31,7 @@ docker run -d -e SERVER_NAME -e DBHOST -e DBPORT -e DBNAME -e DBUSER -e DBPASSWD
 #With ssl
 #docker run -v /var/www/docker_phpbb/phpbb/ssl:/etc/apache2/ssl -e SERVER_NAME -e DBHOST -e DBPORT -e DBNAME -e DBUSER -e DBPASSWD -e TABLE_PREFIX -p 80:80 -p 443:443 apache7.2
 
+mysql -h $DBHOST -P $DBPORT -u phpbb_prod_user -D phpbb_prod_db -p
 
 
 
