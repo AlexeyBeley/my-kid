b3260464b6d85dfd69803ce6e2c1debecaad60f8
Alexey Beley <alexey.beley@gmail.com>
2020-06-01 12:22:38 +0300

    added sleep

--- sections_server/deploy_testing.sh
+++ sections_server/deploy_testing.sh
@@ -13,6 +13,8 @@ fi
 docker pull mysql/mysql-server:5.7
 docker_id=$(docker run --name=$DB_NAME -d -p 3306:3306  mysql/mysql-server:5.7)
 
+sleep 5
+
 pass_line=$(docker logs $docker_id | grep "GENERATED ROOT PASSWORD")
 DB_PASSWORD=$(echo $pass_line | awk '{print $5}')
 export DB_PASSWORD
