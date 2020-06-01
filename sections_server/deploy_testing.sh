29411fcc9cb28fbe4a3679b42e08b1a60c3e95cf
Alexey Beley <alexey.beley@gmail.com>
2020-06-01 12:10:42 +0300

    fix find exited contianers

--- sections_server/deploy_testing.sh
+++ sections_server/deploy_testing.sh
@@ -3,7 +3,7 @@
 #create_database docker
 export DB_NAME="sections_db_test"
 
-running_container_id=$(docker container ls -all  -f "name=$DB_NAME")
+running_container_id=$(docker container ls --all -q -f name=$DB_NAME)
 if [[ !  -z  $running_container_id""  ]]
 then
   docker kill $running_container_id
